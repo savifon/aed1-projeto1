c7b19017fe19d22a936208cd4c7187b236506386
Savio Fonseca <savifon1@gmail.com>
2020-09-28 19:27:45 +0000

    Initial commit

---
+++ Empresa.cs
@@ -0,0 +1,53 @@
+using System;
+
+class Empresa {
+	private string RazaoSocial;
+	private string Cnpj;
+	private string Servicos;
+	private string Dados;
+
+	public void SetRazaoSocial(string rs) {
+		if (String.IsNullOrEmpty(rs))
+			Console.WriteLine("A Razão Social informada é nula ou vazia.");
+		else
+			RazaoSocial = rs;
+	}
+
+	public string GetRazaoSocial() {
+		return RazaoSocial;
+	}
+
+	public void SetCnpj(string n) {
+		if (String.IsNullOrEmpty(n))
+			Console.WriteLine("O CNPJ informado é nulo ou vazio.");
+		else
+			Cnpj = n;
+	}
+
+	public string GetCnpj() {
+		return Cnpj;
+	}
+
+	public void SetServicos(string s) {
+		if (String.IsNullOrEmpty(s))
+			Console.WriteLine("Os serviços informados são nulos ou vazios.");
+		else
+			Servicos = s;
+	}
+
+	public string GetServicos() {
+		return Servicos;
+	}
+
+	public void SetDados(string d) {
+		if (String.IsNullOrEmpty(d))
+			Console.WriteLine("Os dados informados são nulos ou vazios.");
+		else
+			Servicos = sd;
+	}
+
+	public string GetServicos() {
+		return Servicos;
+	}
+
+}
